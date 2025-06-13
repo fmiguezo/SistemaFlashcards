d106b40804d20ea7a1ee64c5d15d5d9a935fa199
juanpablo12398 <jpverdondoni@gmail.com>
2025-06-12 22:10:23 -0300

    Cambios varios fire base

---
+++ src/main/java/edu/utn/infrastructure/config/FirebaseConfig.java
@@ -0,0 +1,31 @@
+package edu.utn.infrastructure.config;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+import javax.annotation.PostConstruct;
+import java.io.InputStream;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void init() throws Exception {
+
+        // 1. Se carga la clave de servicio de Firebase
+        InputStream serviceAccount = getClass()
+                .getClassLoader()
+                .getResourceAsStream("service-account.json");
+
+        // 2. Se inicializa la aplicación Firebase con las credenciales
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://flashcard-ea570-default-rtdb.firebaseio.com")
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+            System.out.println("✅ Firebase inicializado correctamente.");
+        }
+    }
+}
